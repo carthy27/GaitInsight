8a79b44a8f8edd62a64b5b86219e6c1f87d2005d
carthy27 <03thomasmc@gmail.com>
2025-03-21 13:54:51 +0000

    Trying to add Auth

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,12 @@
 import { ExpoRoot } from "expo-router";
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
-  return  <ExpoRoot />;
+  const ctx = require.context('./', true, /.*/);
+  
+  return (
+    <AuthProvider>
+      <ExpoRoot context={ctx} />
+    </AuthProvider>
+  );
 }
--- app/component/SignIn.tsx
+++ app/component/SignIn.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { View, Button,  Alert } from "react-native";
-import styles from "../styles/SingIn"; 
+import { View, Button, Alert } from "react-native";
+import styles from "../styles/SingIn";
 import { Link } from "expo-router";
 import GoogleLogo from "../assets/google";
+import { useAuth } from '../context/AuthContext';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
+
 export default function SignIn() {
+  const { user } = useAuth();
+
+  const handleSignIn = async (email: string, password: string) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error('Sign in error:', error);
+    }
+  };
+
   const handlePress = () => {
     Alert.alert("Button Pressed", "You pressed the sign-in button!");
   };
 
   return (
     <View style={styles.container}>
+      <Button title="Sign In" onPress={handlePress} />
       <Link href="/about" style={styles.button}>About  </Link>
       {/* <Link href="/Register"><GoogleLogo  /> </Link>   */}
       <Link href="/Register" style={styles.button}>Register</Link>
