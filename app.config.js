5ac4162b63ba41390457ec45f22db9f37b32f147
carthy27 <03thomasmc@gmail.com>
2025-03-23 20:03:50 +0000

    Gotta be now

--- app.config.js
+++ app.config.js
@@ -1,15 +1,46 @@
+import { version } from './package.json';
+
 export default {
   expo: {
     name: 'GaitInsight',
     slug: 'gaitinsight',
     scheme: 'gaitinsight',
-    version: '1.0.0',
+    version,
     orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
     web: {
       bundler: 'metro',
       output: 'static',
       headers: {
         'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+      }
+    },
+    android: {
+      package: "com.gaitinsight.firebasestorage",
+      googleServicesFile: "./google-services.json",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      permissions: [
+        "CAMERA",
+        "INTERNET",
+        "ACCESS_NETWORK_STATE"
+      ]
+    },
+    plugins: [
+      "expo-camera",
+      "@react-native-google-signin/google-signin"
+    ],
+    extra: {
+      eas: {
+        projectId: "dccece7a-cc19-4352-a538-6e66bcc10d16"
       }
     }
   }
